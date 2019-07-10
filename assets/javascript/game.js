78a400772b458d28978eadb7a6f68768f48ff3ab
meowster93 <cmendoza6277@yahoo.com>
2019-07-10 03:09:01 -0400

    index and style sheet were changed and styled to fit mvc needs and added functions to +/- in game.js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,15 +1,30 @@
 $(document).ready(function () {
     //creating local variable
     var playerLife = 20;
-    var opponentLife = 20;
+    var oppLife = 20;
 
     //display life total
     $("#lifeNumber").text("" + playerLife);
-    $("#lifeOpp").text("" + opponentLife);
+    $("#lifeOpp").text("" + oppLife);
 
     //create on click function for add/subtract to life total
     $("#playerAdd").on("click", function() {
         playerLife++;
         $("#lifeNumber").text("" + playerLife)
+    }); 
+
+    $("#playerSub").on("click", function() {
+        playerLife--;
+        $("#lifeNumber").text("" + playerLife)
+    }); 
+
+    $("#oppAdd").on("click", function() {
+        oppLife++;
+        $("#lifeOpp").text("" + oppLife)
+    }); 
+
+    $("#oppSub").on("click", function() {
+        oppLife--;
+        $("#lifeOpp").text("" + oppLife)
     }); 
 });
